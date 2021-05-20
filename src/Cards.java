9e56a102a9f47ef0eafbd5771538777e93cc24bb
Cruz Gonzalez <cruzgonzalez21@gmail.com>
2021-05-20 11:19:51 -0700

    added initial code

---
+++ src/Cards.java
@@ -0,0 +1,31 @@
+
+public class Cards {
+
+	private int value = 0;
+	private String name = "";
+	
+	public Cards(); {
+		value = 0;
+		name = "";
+	}
+	
+	
+	
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+}
